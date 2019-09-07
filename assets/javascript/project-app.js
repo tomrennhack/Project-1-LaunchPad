4f23800b7c528e8502e8786f1674c2731b8ab97e
Your Name <sam@upsidebusinessgroup.com.au>
2019-09-07 10:48:48 -0400

    added hyperlinks

--- assets/javascript/project-app.js
+++ assets/javascript/project-app.js
@@ -15,15 +15,19 @@ $("#search-country").on("click", function () {
         for (var i = 0; i < response.items.length; i++) {
 
             var imgURL = response.items[i].snippet.thumbnails.default.url;
+            var videoURL = response.items[i].id.videoId;
             console.log(imgURL)
+            console.log(response)
+            console.log(videoURL)
 
             var imgDiv = $("<div>");
+            
 
             // Storing the result title
             var Title = response.items[i].snippet.title;
 
             // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Title: " + Title);
+            var a = $("<a>").text("Title: " + Title);
 
             // Creating an image tag
            
@@ -32,11 +36,14 @@ $("#search-country").on("click", function () {
             // Giving the image tag an src attribute of a proprty pulled off the
             // result item
             videoImage.attr("src", response.items[i].snippet.thumbnails.default.url);
+            videoImage.attr("href", "https://www.youtube.com/watch?v="+videoURL);
+            a.attr("href", "https://www.youtube.com/watch?v="+videoURL);
+            
 
             // Appending the paragraph and personImage we created to the "gifDiv" div we created
            
             imgDiv.append(videoImage);
-            imgDiv.append(p);
+            imgDiv.append(a);
 
             // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
             $("#youtube-results").append(imgDiv);
