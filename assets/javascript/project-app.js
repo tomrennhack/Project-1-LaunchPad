630802bc58cf9ce45beb82f884c97653c1c3a40a
Your Name <sam@upsidebusinessgroup.com.au>
2019-09-07 11:41:56 -0400

    added music videos & cleared results on refresh

--- assets/javascript/project-app.js
+++ assets/javascript/project-app.js
@@ -1,9 +1,9 @@
-// youtube query
+// return video results
 
 $("#search-country").on("click", function () {
 
     var countryName = $("#country-input").val().trim()
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=" + countryName + "+info&type=video&videoDefinition=high&key=AIzaSyBLL2tHYwaiSDhxVoFeqHUjOqhUbuV3HRY"
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=" + countryName + "+info+video&type=video&videoDefinition=high&key=AIzaSyBLL2tHYwaiSDhxVoFeqHUjOqhUbuV3HRY"
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -11,6 +11,7 @@ $("#search-country").on("click", function () {
 
         console.log("workingagain");
         console.log(response.items.length)
+        $("#youtube-results").empty();
 
         for (var i = 0; i < response.items.length; i++) {
 
@@ -20,8 +21,10 @@ $("#search-country").on("click", function () {
             console.log(response)
             console.log(videoURL)
 
+
+            // create var for thumbnail image
             var imgDiv = $("<div>");
-            
+
 
             // Storing the result title
             var Title = response.items[i].snippet.title;
@@ -30,23 +33,80 @@ $("#search-country").on("click", function () {
             var a = $("<a>").text("Title: " + Title);
 
             // Creating an image tag
-           
+
             var videoImage = $("<img>");
 
             // Giving the image tag an src attribute of a proprty pulled off the
             // result item
             videoImage.attr("src", response.items[i].snippet.thumbnails.default.url);
-            videoImage.attr("href", "https://www.youtube.com/watch?v="+videoURL);
-            a.attr("href", "https://www.youtube.com/watch?v="+videoURL);
-            
+            videoImage.attr("href", "https://www.youtube.com/watch?v=" + videoURL);
+            a.attr("href", "https://www.youtube.com/watch?v=" + videoURL);
+
 
             // Appending the paragraph and personImage we created to the "gifDiv" div we created
-           
+
             imgDiv.append(videoImage);
             imgDiv.append(a);
 
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+            // Appending the gifDiv to the "#gifs-appear-here" div in the HTML
             $("#youtube-results").append(imgDiv);
+        }
+    });
+})
+
+
+// return music video results
+
+$("#search-country").on("click", function () {
+
+    var countryName = $("#country-input").val().trim()
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=" + countryName + "+music+video&type=video&videoDefinition=high&key=AIzaSyBLL2tHYwaiSDhxVoFeqHUjOqhUbuV3HRY"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log("workingagain");
+        console.log(response.items.length)
+        $("#spotify-results").empty();
+
+        for (var i = 0; i < response.items.length; i++) {
+
+            var imgURL = response.items[i].snippet.thumbnails.default.url;
+            var videoURL = response.items[i].id.videoId;
+            console.log(imgURL)
+            console.log(response)
+            console.log(videoURL)
+
+
+            // create var for thumbnail image
+            var imgDiv = $("<div>");
+
+
+            // Storing the result title
+            var Title = response.items[i].snippet.title;
+
+            // Creating a paragraph tag with the result item's rating
+            var a = $("<a>").text("Title: " + Title);
+
+            // Creating an image tag
+
+            var videoImage = $("<img>");
+
+            // Giving the image tag an src attribute of a proprty pulled off the
+            // result item
+            videoImage.attr("src", response.items[i].snippet.thumbnails.default.url);
+            videoImage.attr("href", "https://www.youtube.com/watch?v=" + videoURL);
+            a.attr("href", "https://www.youtube.com/watch?v=" + videoURL);
+
+
+            // Appending the paragraph and personImage we created to the "gifDiv" div we created
+
+            imgDiv.append(videoImage);
+            imgDiv.append(a);
+
+            // Appending the gifDiv to the "#gifs-appear-here" div in the HTML
+            $("#spotify-results").append(imgDiv);
         }
     });
 })
