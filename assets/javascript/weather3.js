a3d7e63a45050fe8d08b1d22f0e69629b9451bf2
sinuhe Montero <sinuhem23@gmail.com>
2019-09-10 03:03:09 -0400

    updated some more and changed a few things

--- assets/javascript/weather3.js
+++ assets/javascript/weather3.js
@@ -13,16 +13,16 @@ $("#search-country").on("click", function () {
     }).done(function (json) {
         if (json.success == true) {
             var ob = json.response.ob;
-            // Weather
-            $('#weather-results').html('The current weather is ' + ob.weather + ' <br> Currently ' + ob.tempF + '&deg;' + 'F' + '<br> Feels like' + ob.feelslikeF + 'F' + '<br> Currently ' + ob.tempC + '&deg;' + 'C' + '<br> Feels like' + ob.feelslikeC + 'C' + '<br> The Humidity is: ' + ob.humidity + '%');
-            // TimeStamp
-            $('#time-results').html('Date and time is ' + ob.dateTimeISO + ' <br> Sunrise with data/tme: ' + ob.sunriseISO + '<br> Sunset with date/time: ' + ob.sunsetISO);
-        
+             // Weather
+             $('#weather-results').html('The current weather is ' + ob.weather + '<br>' + ' <br> Currently ' + ob.tempF + '&deg;' + 'F' + '<br> Feels like ' + ob.feelslikeF +  '&deg;' + 'F' + '<br>' + '<br> Currently ' + ob.tempC + '&deg;' + 'C' + '<br> Feels like ' + ob.feelslikeC + '&deg;'  + 'C' +  '<br>' + '<br> The Humidity is: ' + ob.humidity + '%');
+             // TimeStamp
+             $('#time-results').html('Date and time is ' + '<br>' + ob.dateTimeISO +  '<br>' + ' <br> Sunrise with data/time: ' + '<br>' + ob.sunriseISO + '<br>' +  '<br> Sunset with date/time: ' + '<br>' + ob.sunsetISO);
+         
         }
+
          else {
-            alert('For the weather, please type (City),(either State, Provience, or Country): ' + json.error.description);
+            alert('For the weather, please type (City),(either State, Provience, or Country): ');
          }
      });
 });
-
-
+       
